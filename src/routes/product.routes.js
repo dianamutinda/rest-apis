67c02a1d76e9747785f9e16193189111304b6988
dianamutinda <jaydiana25@gmail.com>
2024-06-23 10:03:23 +0300

    implement the get all products route

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,8 +1,14 @@
 import { Router} from 'express'
+import pool from '../db.config.js';
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("get all products")
+router.get("/", async (req, res) => {
+    try {
+       const result = await pool.query("SELECT * FROM products");
+       res.status(200).json({success:true, data: result.rows});
+    } catch (error) {
+        res.status(500).json({success:false, message: error.message});
+    }
 })
 
 router.get("/:id", (req, res) => {
